5b641c7f810b5f8e2bdf31abdb7dff8820c164a6
Roberto J. Angulo <robeangulo25@gmail.com>
2016-12-17 12:49:41 -0500

    my fisrt commit

--- App1/ViewController.swift
+++ App1/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var dalabel: UILabel!
+// texto de hello world
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var tapCount = 0
+    @IBAction func a(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        print (tapCount)
+        
+        if tapCount >= 10 {
+            dalabel.text = "wujuuu"
+        }
+        // cambio de texto al oprimir botton
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
